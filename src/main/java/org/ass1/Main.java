dcd807207d766b0f6c08ad694b185d09dc9a23a9
WentingLi-20021908 <liwentingcoco@163.com>
2022-08-10 22:12:02 +1200

    init project

--- src/main/java/org/ass1/Main.java
+++ src/main/java/org/ass1/Main.java
@@ -1,4 +1,10 @@
 package org.ass1;
 
 public class Main {
+    // Main class
+    public static void main(String args[])
+    {
+        TextFrame e = new TextFrame();
+    }
+
 }
---
+++ src/main/java/org/ass1/TextFrame.java
@@ -0,0 +1,22 @@
+package org.ass1;// Java Program to create a text editor using java
+
+import javax.swing.*;
+
+class TextFrame extends JFrame {
+
+    // Frame
+    JFrame frame;
+
+    // Constructor
+    TextFrame()
+    {
+        // Create a frame
+        frame = new JFrame("Test Editor");
+
+        frame.setSize(500, 500);
+        frame.show();
+    }
+
+
+
+}
